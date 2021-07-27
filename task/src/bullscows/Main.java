bb9534907316fcf1960576b438673fecc0d5c765
Jeremy Grecu <jeremy.grecu@sap.com>
2021-07-27 16:17:13 +0100

    Bulls and Cows - Stage 1/7: Game log

--- task/src/bullscows/Main.java
+++ task/src/bullscows/Main.java
@@ -2,6 +2,13 @@ package bullscows;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("The secret code is prepared: ****.");
+        System.out.println("Turn 1. Answer:\n" +
+                "1234\n" +
+                "Grade: None.\n");
+        System.out.println("Turn 2. Answer:\n" +
+                "9876\n" +
+                "Grade: 4 bulls.");
+        System.out.println("Congrats! The secret code is 9876.");
     }
 }
