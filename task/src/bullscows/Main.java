23bdc3db37f31abba586d9418ffa4e42f262c39b
Jeremy Grecu <jeremy.grecu@sap.com>
2021-07-29 13:25:17 +0100

    JBA - Bulls and Cows - Stage 6/7: New level

--- task/src/bullscows/Main.java
+++ task/src/bullscows/Main.java
@@ -4,23 +4,40 @@ import java.util.Scanner;
 
 public class Main {
     static final Scanner scanner = new Scanner(System.in);
+    static final int MAX_SIZE = 36;
+    static final String digits = "0123456789abcdefghijklmnopqrstuvwxyz";
+
     public static void main(String[] args) {
         int turn = 0;
         int secretLength;
+        int symbolsLength;
 
         while (true) {
             System.out.println("Please, enter the secret code's length: ");
 
             secretLength =  Integer.parseInt(scanner.nextLine());
-            if (secretLength < 11) {
+            if (secretLength <= MAX_SIZE) {
                 break;
             } else {
                 System.out.println("Error: can't generate a secret number with a length of " + secretLength +
                         " because there aren't enough unique digits.");
             }
         }
+        while (true) {
+            System.out.println("Input the number of possible symbols in the code: ");
+
+            symbolsLength =  Integer.parseInt(scanner.nextLine());
+            if (symbolsLength <= MAX_SIZE) {
+                break;
+            } else {
+                System.out.println("Error: can't generate a secret number with a length of " + symbolsLength +
+                        " because there aren't enough unique digits.");
+            }
+        }
+
+        String randomSecret = getRandomSecret(secretLength, symbolsLength);
+        printSecret(secretLength, symbolsLength);
         System.out.println("Okay, let's start a game!");
-        String randomSecret = getRandomSecret(secretLength);
         String guess = "";
         while (!guess.equals(randomSecret)) {
             turn++;
@@ -60,27 +77,43 @@ public class Main {
         }
     }
 
-    public static String getRandomSecret(int secretLength) {
-        StringBuilder sb = new StringBuilder();
-        while (true) {
-            long pseudoRandomNumber = System.nanoTime();
-            String pseudoRandNumStr = Long.valueOf(pseudoRandomNumber).toString();
-            sb.setLength(0);
-            for (int i = 0; i < pseudoRandNumStr.length() - 4; i++) {
-                String digit = Character.valueOf(pseudoRandNumStr.charAt(i + 4)).toString();
-                if (sb.length() == 0 && digit.equals("0")) {
-                    continue;
-                } else if (sb.indexOf(digit) < 0) {
-                    sb.append(digit);
-                }
-                if (sb.length() == secretLength) {
-                    break;
-                }
+    static String getRandomSecret(int length, int symbols){
+        StringBuilder result = new StringBuilder();
+        int random;
+
+        while(true){
+            random  = (int) (Math.random() * symbols);
+            char charAt = digits.charAt(random);
+            if (result.length() == 0 && charAt == '0'){
+                //if it starts with 0, try again.
+                continue;
+            } else if (!result.toString().contains(Character.valueOf(charAt).toString())){
+                result.append(charAt);
             }
-            if (sb.length() == secretLength) {
+            if(result.length() == length){
                 break;
             }
         }
-        return sb.toString();
+
+        return result.toString();
+    }
+
+    static void printSecret(int length, int symbols) {
+        StringBuilder sb = new StringBuilder();
+        char c = digits.charAt(symbols - 1);
+        sb.append("*".repeat(length));
+        sb.append(" ");
+        if (symbols < 10) {
+            sb.append("(0-");
+            sb.append(c);
+            sb.append(").");
+        } else if (symbols == 10) {
+            sb.append("(0-9, a).");
+        } else {
+            sb.append("(0-9, a-");
+            sb.append(c);
+            sb.append(").");
+        }
+        System.out.println("The secret is prepared: " + sb);
     }
 }
